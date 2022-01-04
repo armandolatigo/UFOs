32f7caaf1410bd9f4187af5cfdb9a538a465c308
armandolatigo <92451164+armandolatigo@users.noreply.github.com>
2022-01-04 17:16:17 -0600

    mindor change to app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const tableData = data;
 // Reference the HTML table using D3
 var body = d3.select("tbody");
 
-// Simple JavaScript console.log statement
-//function printHello() {
-//    console.log("Hello there!");
-//}
+function buildTable(data) {
+
+}
