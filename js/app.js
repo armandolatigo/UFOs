e2b6003a8757543cffa42ea03093c69a749a5fe6
armandolatigo <92451164+armandolatigo@users.noreply.github.com>
2022-01-03 23:39:04 -0600

    new files added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using D3
+var body = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+//function printHello() {
+//    console.log("Hello there!");
+//}
